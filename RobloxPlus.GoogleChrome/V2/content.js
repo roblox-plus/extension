bc878d34ae2eda1acd8ef8d0d79e3bc7353cd7a0
WebGL3D <OgreLandBob@gmail.com>
2017-02-26 03:23:34 -0800

    Split ShowForum out of content.js

--- RobloxPlus.GoogleChrome/V2/content.js
+++ RobloxPlus.GoogleChrome/V2/content.js
@@ -1061,33 +1061,6 @@ $(".MediaPlayerIcon[data-mediathumb-url]").each(function(){$(this).replaceWith(s
 			}
 		},100);
 	}
-},{
-	match: [/^\/Forum\/ShowForum\.aspx/i,/^\/Forum\/User\/MyForums\.aspx/i],
-	fill: function(){
-		$("div[class='thread-link-outer-wrapper']>div").css("max-width",($("#Body").width()/4)+"px");
-		storage.get("forums",function(f){
-			if(f.blocking){
-				friendService.blocked(function(b){
-					$(".post-list-author").each(function(){
-						if(b.data[users.urlId($(this).attr("href"))]){
-							$(this).parent().parent().hide();
-						}
-					});
-				});
-			}
-		});
-		
-		var trackLab = $("#ctl00_cphRoblox_MyForums1>table>tbody>tr:nth-child(3)>td>h2");
-		if(trackLab.length){
-			trackLab.text("Your Tracked Threads ("+$("#ctl00_cphRoblox_MyForums1_ctl00_ThreadTracking>tbody>.forum-table-row").each(function(){
-				var id = Number(url.param("postid",$(this).find(".post-list-subject").attr("href")));
-				$(this).find(">td:first-child>img").replaceWith($("<input type=\"checkbox\" checked=\"checked\">").change(function(){
-					forumService[$(this).prop("checked")?"track":"untrack"](id,function(){});
-					trackLab.text("Your Tracked Threads ("+$("#ctl00_cphRoblox_MyForums1_ctl00_ThreadTracking>tbody>.forum-table-row>td>input:checked").length+")");
-				}));
-			}).length+")");
-		}
-	}
 }
 ]);
 
---
+++ RobloxPlus.GoogleChrome/V2/content/pages/forum/ShowForum.js
@@ -0,0 +1,35 @@
+﻿var RPlus = RPlus || {};
+RPlus.Pages = RPlus.Pages || {};
+
+RPlus.Pages.ShowForum = function () {
+	$("div[class='thread-link-outer-wrapper']>div").css("max-width", ($("#Body").width() / 4) + "px");
+	storage.get("forums", function (f) {
+		if (f.blocking) {
+			friendService.blocked(function (b) {
+				$(".post-list-author").each(function () {
+					if (b.data[users.urlId($(this).attr("href"))]) {
+						$(this).parent().parent().hide();
+					}
+				});
+			});
+		}
+	});
+
+	var trackLab = $("#ctl00_cphRoblox_MyForums1>table>tbody>tr:nth-child(3)>td>h2");
+	if (trackLab.length) {
+		trackLab.text("Your Tracked Threads (" + $("#ctl00_cphRoblox_MyForums1_ctl00_ThreadTracking>tbody>.forum-table-row").each(function () {
+			var id = Number(url.param("postid", $(this).find(".post-list-subject").attr("href")));
+			$(this).find(">td:first-child>img").replaceWith($("<input type=\"checkbox\" checked=\"checked\">").change(function () {
+				forumService[$(this).prop("checked") ? "track" : "untrack"](id, function () { });
+				trackLab.text("Your Tracked Threads (" + $("#ctl00_cphRoblox_MyForums1_ctl00_ThreadTracking>tbody>.forum-table-row>td>input:checked").length + ")");
+			}));
+		}).length + ")");
+	}
+
+	return {};
+};
+
+RPlus.Pages.ShowForum.patterns = [/^\/Forum\/ShowForum\.aspx/i, /^\/Forum\/User\/MyForums\.aspx/i];
+
+
+// WebGL3D
