3d0b109d7fef36250335e30d2ef2ab4f2f119435
WebGL3D <OgreLandBob@gmail.com>
2017-06-04 16:12:17 -0700

    Convert friend notifier to RPlus.notifiers base

--- RobloxPlus.GoogleChrome/V2/background/notifiers/friendNotifier.js
+++ RobloxPlus.GoogleChrome/V2/background/notifiers/friendNotifier.js
@@ -1,68 +1,6 @@
 ﻿/* background/notifiers/friendNotifier.js [06/04/2017] */
-
-var RPlus = RPlus || {};
-RPlus.notifiers = RPlus.notifiers || {};
-// Leaving friendNotifier for legacy reasons.
-// TODO: Delete friendNotifier accessor
-RPlus.notifiers.friend = friendNotifier = RPlus.notifiers.friend || (function () {
-	var friendNotifier;
-	friendNotifier = setupNotifier(function (loop, uid) {
-		var fn = storage.get("friendNotifier") || {};
-		if (!fn.on) {
-			loop();
-			return;
-		}
-		var startup = friendNotifier.ran != uid;
-		if (startup) {
-			friendNotifier.cache.clear();
-		}
-		friendNotifier.getFriendsWithPresence(uid).then(function (list) {
-			friendNotifier.ran = uid;
-			var tag = [];
-			list.forEach(function (friend) {
-				tag.push(friend.id);
-				var old = friendNotifier.cache.get(friend.id);
-				if (!startup && (type(fn.blocked) != "array" || fn.blocked.indexOf(friend.id) < 0)) {
-					if (!old) {
-						//friendNotifier.clicknote(o,"You and "+o.username+" are now friends!");
-					} else if (fn.online && (old.isOnline != friend.isOnline) && friend.isOnline) {
-						friendNotifier.clicknote(friend, friend.username + " is now online");
-					} else if (fn.offline && (old.isOnline != friend.isOnline) && !friend.isOnline) {
-						friendNotifier.clicknote(friend, friend.username + " is now offline");
-					} else if (fn.game && friend.game && (!old.game || old.game.serverId != friend.game.serverId)) {
-						note = notify({
-							header: friend.username + " joined a game",
-							lite: friend.game.name,
-							icon: Roblox.thumbnails.getUserHeadshotThumbnailUrl(friend.id, 4),
-							buttons: ["Follow"],
-							robloxSound: Number((storage.get("notifierSounds") || {}).friend) || 0,
-							tag: "friend" + friend.id,
-							url: {
-								url: Roblox.users.getProfileUrl(friend.id),
-								close: true
-							},
-							clickable: true
-						}).button1Click(function () {
-							Roblox.games.launch({
-								followUserId: friend.id
-							});
-						});
-					}
-				}
-				friendNotifier.cache.set(friend.id, friend);
-			});
-			for (var n in friendNotifier.cache.data) {
-				if (!tag.includes(Number(n))) {
-					delete friendNotifier.cache.data[n];
-				}
-			}
-			loop();
-		}, loop);
-	}, {
-			userId: true
-		});
-
-	friendNotifier.clicknote = function (friend, header, note) {
+RPlus.notifiers.friends = (function () {
+	function clicknote(friend, header, note) {
 		return note = notify({
 			header: header,
 			icon: Roblox.thumbnails.getUserHeadshotThumbnailUrl(friend.id, 4),
@@ -76,7 +14,7 @@ RPlus.notifiers.friend = friendNotifier = RPlus.notifiers.friend || (function ()
 		});
 	};
 
-	friendNotifier.getFriendsWithPresence = function (userId) {
+	function getFriendsWithPresence(userId) {
 		return new Promise(function (resolve, reject) {
 			Roblox.social.getFriends(userId).then(function (friends) {
 				var friendIds = [];
@@ -100,13 +38,67 @@ RPlus.notifiers.friend = friendNotifier = RPlus.notifiers.friend || (function ()
 		});
 	};
 
-	friendNotifier.ran = 0;
-	friendNotifier.cache = compact.cache(0);
-	friendNotifier.run();
 
-	return friendNotifier;
+	return RPlus.notifiers.init({
+		name: "Friends",
+		sleep: 10 * 1000,
+		isEnabled: function (callBack) {
+			callBack((storage.get("friendNotifier") || {}).on);
+		},
+		requireAuthenticatedUser: true
+	}, function (user, cache, rerun) {
+		return new Promise(function (resolve, reject) {
+			var fn = storage.get("friendNotifier") || {};
+
+			getFriendsWithPresence(user.id).then(function (list) {
+				var tag = [];
+
+				list.forEach(function (friend) {
+					tag.push(friend.id);
+					var old = cache[friend.id];
+
+					if (rerun && (!Array.isArray(fn.blocked) || !fn.blocked.includes(friend.id))) {
+						if (!old) {
+							//clicknote(o,"You and "+o.username+" are now friends!");
+						} else if (fn.online && (old.isOnline != friend.isOnline) && friend.isOnline) {
+							clicknote(friend, friend.username + " is now online");
+						} else if (fn.offline && (old.isOnline != friend.isOnline) && !friend.isOnline) {
+							clicknote(friend, friend.username + " is now offline");
+						} else if (fn.game && friend.game && (!old.game || old.game.serverId !== friend.game.serverId)) {
+							note = notify({
+								header: friend.username + " joined a game",
+								lite: friend.game.name,
+								icon: Roblox.thumbnails.getUserHeadshotThumbnailUrl(friend.id, 4),
+								buttons: ["Follow"],
+								robloxSound: Number((storage.get("notifierSounds") || {}).friend) || 0,
+								tag: "friend" + friend.id,
+								url: {
+									url: Roblox.users.getProfileUrl(friend.id),
+									close: true
+								},
+								clickable: true
+							}).button1Click(function () {
+								Roblox.games.launch({
+									followUserId: friend.id
+								});
+							});
+						}
+					}
+
+					cache[friend.id] = friend;
+				});
+
+				for (var n in cache.data) {
+					if (!tag.includes(Number(n))) {
+						delete cache.data[n];
+					}
+				}
+
+				resolve([]);
+			}, reject);
+		});
+	});
 })();
-
 
 
 // WebGL3D
