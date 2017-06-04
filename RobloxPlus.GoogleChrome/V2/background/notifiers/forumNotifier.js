8a26d00c6194628f6667e6ebb5717cb21b00badb
WebGL3D <OgreLandBob@gmail.com>
2017-06-04 16:00:20 -0700

    Convert forum notifier to RPlus.notifiers base

--- RobloxPlus.GoogleChrome/V2/background/notifiers/forumNotifier.js
+++ RobloxPlus.GoogleChrome/V2/background/notifiers/forumNotifier.js
@@ -1,104 +1,91 @@
 ﻿/* background/notifiers/forumNotifier.js [06/04/2017] */
-
-var RPlus = RPlus || {};
-RPlus.notifiers = RPlus.notifiers || {};
-// Leaving forumNotifier for legacy reasons.
-// TODO: Delete forumNotifier accessor
-RPlus.notifiers.forum = forumNotifier = RPlus.notifiers.forum || (function () {
-	var forumNotifier;
-	var threadCache = {};
-
-	forumNotifier = setupNotifier(function (loop, uid) {
-		var startup = forumNotifier.ran != uid;
-		if (startup) {
-			threadCache = {};
-		}
-		var blacklist = (storage.get("forums") || {}).blacklist;
-		if (type(blacklist) != "array") {
-			blacklist = [];
-		}
-		var dcb = 0;
-		var mcb = 1;
-		var tracked = [];
-		var fcb = function () {
-			if (++dcb == mcb) {
-				loop();
+RPlus.notifiers.forum = (function () {
+	return RPlus.notifiers.init({
+		name: "Forum",
+		sleep: 25 * 1000,
+		isEnabled: function (callBack) {
+			callBack(storage.get("forumNotifier"));
+		},
+		requireAuthenticatedUser: true
+	}, function (user, cache, rerun) {
+		return new Promise(function (resolve, reject) {
+			var blacklist = (storage.get("forums") || {}).blacklist;
+			if (type(blacklist) != "array") {
+				blacklist = [];
 			}
-		};
-		Roblox.users.getAuthenticatedUser().then(function (authenticatedUser) {
-			mcb++;
+
+			var dcb = 0;
+			var mcb = 2;
+			var tracked = [];
+			var fcb = function () {
+				if (++dcb == mcb) {
+					resolve([]);
+				}
+			};
+			
 			function handleForumData(data, isTrackedThreads) {
-				forumNotifier.ran = uid;
 				data.forEach(function (thread) {
 					// If we're not starting up, and the thread is:
 					// Just appearing and isn't a brand new post or something we just tracked
 					// Has a new reply than we've seen before
 					// not something we just posted
 					// not blacklisted
-					if (!startup && (
-						(!threadCache.hasOwnProperty(thread.id) && !isTrackedThreads)
-						|| (threadCache.hasOwnProperty(thread.id) && threadCache[thread.id].lastReply.id != thread.lastReply.id))
-						&& thread.lastReply.poster != authenticatedUser.username
+					if (rerun && (
+						(!cache.hasOwnProperty(thread.id) && !isTrackedThreads)
+						|| (cache.hasOwnProperty(thread.id) && cache[thread.id].lastReply.id !== thread.lastReply.id))
+						&& thread.lastReply.poster !== user.username
 						&& !blacklist.includes(thread.id)) {
 						mcb++;
 						Roblox.forum.getForumThreadReplies(thread.id, thread.lastReply.page).then(function (post) {
-							var o;
-							for (var n in post.data) {
-								if (post.data[n].id == thread.lastReply.id) {
-									var reply = post.data[n];
-									var note = {
-										header: string.clean(reply.poster.username + " replied to thread\n" + post.subject.substring(0, 50)),
-										lite: string.clean(reply.body.split("\n")[0]),
-										icon: Roblox.thumbnails.getUserHeadshotThumbnailUrl(reply.poster.id, 3),
-										buttons: ["Reply"],
-										clickable: true,
-										robloxSound: Number((storage.get("notifierSounds") || {}).forum) || 0,
-										tag: "forum" + reply.id,
-										url: {
-											url: "https://forum.roblox.com/Forum/ShowPost.aspx?PostID=" + reply.id + (thread.lastReply.page != 1 ? "&PageIndex=" + thread.lastReply.page : "") + "#" + reply.id,
-											close: true
-										}
-									};
-									if (!note.robloxSound) {
-										delete note.robloxSound;
-										note.speak = note.header.split(/\n/)[0];
-									}
-									note = notify(note).button1Click(function () {
-										window.open("https://forum.roblox.com/Forum/AddPost.aspx?mode=flat&PostID=" + reply.id);
-										note.close();
-									});
-									break;
+							post.data.forEach(function (reply) {
+								if (reply.id !== thread.lastReply.id) {
+									return;
 								}
-							}
+
+								var note = {
+									header: string.clean(reply.poster.username + " replied to thread\n" + post.subject.substring(0, 50)),
+									lite: string.clean(reply.body.split("\n")[0]),
+									icon: Roblox.thumbnails.getUserHeadshotThumbnailUrl(reply.poster.id, 3),
+									buttons: ["Reply"],
+									clickable: true,
+									robloxSound: Number((storage.get("notifierSounds") || {}).forum) || 0,
+									tag: "forum" + reply.id,
+									url: {
+										url: "https://forum.roblox.com/Forum/ShowPost.aspx?PostID=" + reply.id + (thread.lastReply.page != 1 ? "&PageIndex=" + thread.lastReply.page : "") + "#" + reply.id,
+										close: true
+									}
+								};
+
+								if (!note.robloxSound) {
+									delete note.robloxSound;
+									note.speak = note.header.split(/\n/)[0];
+								}
+
+								note = notify(note).button1Click(function () {
+									window.open("https://forum.roblox.com/Forum/AddPost.aspx?mode=flat&PostID=" + reply.id);
+									note.close();
+								});
+							});
+
 							fcb();
 						}, fcb);
 					}
-					threadCache[thread.id] = thread;
+
+					cache[thread.id] = thread;
 				});
 				fcb();
 			}
+
 			Roblox.forum.getTrackedThreads().then(function (data) {
-				handleForumData(data.data);
+				handleForumData(data.data, true);
 			}, fcb);
+
 			Roblox.forum.getRecentThreads().then(function (data) {
-				handleForumData(data.data);
+				handleForumData(data.data, false);
 			}, fcb);
-		}, fcb);
-	}, {
-		userId: true,
-		storage: "forumNotifier",
-		interval: 5 * 1000
+		});
 	});
-
-	forumNotifier.ran = 0;
-	forumNotifier.run();
-	forumNotifier.ransack = function (threadId) {
-		delete threadCache[threadId];
-	};
-
-	return forumNotifier;
 })();
-
 
 
 // WebGL3D
