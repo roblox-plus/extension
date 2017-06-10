47cddb2db773e3c08f8d07906a1216a9e615f0b5
WebGL3D <OgreLandBob@gmail.com>
2017-06-10 12:13:08 -0700

    Move context menus into own file

--- RobloxPlus.GoogleChrome/V2/background/background.js
+++ RobloxPlus.GoogleChrome/V2/background/background.js
@@ -81,40 +81,6 @@ if (!ext.incognito) {
 	}, 250);
 }
 
-if (browser.name == "Chrome") {
-	chrome.contextMenus.create({
-		id: "clearNotifications", title: "Clear Notifications", contexts: ["browser_action"], onclick: function () {
-			notification.clear();
-		}
-	});
-
-	chrome.contextMenus.create({
-		id: "mainContext",
-		documentUrlPatterns: ["*://*.roblox.com/*"],
-		title: ext.manifest.name,
-		contexts: ["link"],
-		targetUrlPatterns: ["*://*.roblox.com/users/*/profile"]
-	});
-
-	chrome.contextMenus.create({
-		id: "sendTrade",
-		title: "Trade",
-		contexts: ["link"],
-		targetUrlPatterns: ["*://*.roblox.com/users/*/profile"],
-		documentUrlPatterns: ["*://*.roblox.com/*"],
-		parentId: "mainContext",
-		onclick: function (e) {
-			var id = Roblox.users.getIdFromUrl(e.linkUrl);
-			var u = "https://www.roblox.com/Trade/TradeWindow.aspx?TradePartnerID=" + id;
-			$.get(u).done(function (r) {
-				if (($._(r).find("#aspnetForm[action]").attr("action") || "").endsWith("TradePartnerID=" + id)) {
-					Roblox.trades.openSettingBasedTradeWindow(id);
-				}
-			});
-		}
-	});
-};
-
 
 
 /* Forums */
---
+++ RobloxPlus.GoogleChrome/V2/background/contextMenus.js
@@ -0,0 +1,39 @@
+﻿/* background/contextMenus.js [06/10/2017] */
+chrome.contextMenus.create({
+	id: "clearNotifications",
+	title: "Clear Notifications",
+	contexts: ["browser_action"],
+	onclick: function () {
+		notification.clear();
+	}
+});
+
+chrome.contextMenus.create({
+	id: "mainContext",
+	documentUrlPatterns: ["*://*.roblox.com/*"],
+	title: ext.manifest.name,
+	contexts: ["link"],
+	targetUrlPatterns: ["*://*.roblox.com/users/*/profile"]
+});
+
+chrome.contextMenus.create({
+	id: "sendTrade",
+	title: "Trade",
+	contexts: ["link"],
+	targetUrlPatterns: ["*://*.roblox.com/users/*/profile"],
+	documentUrlPatterns: ["*://*.roblox.com/*"],
+	parentId: "mainContext",
+	onclick: function (e) {
+		var id = Roblox.users.getIdFromUrl(e.linkUrl);
+		$.get("https://www.roblox.com/Trade/TradeWindow.aspx", {
+			TradePartnerID: id
+		}).done(function (r) {
+			if (($._(r).find("#aspnetForm[action]").attr("action") || "").endsWith("TradePartnerID=" + id)) {
+				Roblox.trades.openSettingBasedTradeWindow(id);
+			}
+		});
+	}
+});
+
+
+// WebGL3D
