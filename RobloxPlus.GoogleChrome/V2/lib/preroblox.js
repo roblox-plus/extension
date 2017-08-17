dca6e0a30a20db6b95c55e6d95d670453e402564
WebGL3D <OgreLandBob@gmail.com>
2017-08-16 20:53:03 -0700

    Delete compact (and compact.cache)

--- RobloxPlus.GoogleChrome/V2/lib/preroblox.js
+++ RobloxPlus.GoogleChrome/V2/lib/preroblox.js
@@ -3,29 +3,6 @@
 	For any questions message WebGL3D https://www.roblox.com/messages/compose?recipientId=48103520
 	My messages will always be open for all, if you can't PM me check your settings.
 */
-compact.cache = function (ret, cb) {
-	var timeout = type(ret) == "number" ? ret : 5000;
-	cb = fixCB(cb);
-	return ret = {
-		data: {},
-		get: function (k) { return ret.data[k] && ret.data[k].v; },
-		set: function (k, v, t) {
-			if (ret.data[k] && ret.data[k].t) { clearTimeout(ret.data[k].t); }
-			ret.data[k] = { v: v };
-			if (timeout > 0 && t !== false) {
-				ret.data[k].t = setTimeout(function () {
-					delete ret.data[k];
-				}, timeout);
-			}
-			cb(k, v);
-		},
-		remove: function (k) {
-			delete ret.data[k];
-		},
-		clear: function () { ret.data = {}; }
-	};
-};
-
 url.roblox = {
 	linkify: /(https?\:\/\/)?(?:www\.)?([a-z0-9\-]{2,}\.)*(((m|de|www|web|api|blog|wiki|help|corp|polls|bloxcon|developer|devforum|forum)\.roblox\.com|robloxlabs\.com)|(www\.shoproblox\.com))((\/[A-Za-z0-9-+&@#\/%?=~_|!:,.;]*)|(\b|\s))/gm
 };
