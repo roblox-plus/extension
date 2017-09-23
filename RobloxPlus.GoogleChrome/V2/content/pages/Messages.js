5db685cc679a4384ca724eba3e85f226aba9ba30
WebGL3D <OgreLandBob@gmail.com>
2017-09-23 08:48:52 -0700

    Allow message senders to be dragged over quick info box

---
+++ RobloxPlus.GoogleChrome/V2/content/pages/Messages.js
@@ -0,0 +1,16 @@
+﻿var RPlus = RPlus || {};
+RPlus.Pages = RPlus.Pages || {};
+
+RPlus.Pages.Messages = function () {
+	$("body").on("dragstart", ".roblox-message-row .avatar-card-image", function (e) {
+		var username = $(this).closest(".roblox-message-row").find(".message-summary-username").text();
+		e.originalEvent.dataTransfer.setData("text/plain", "user:" + username);
+	});
+
+	return {};
+};
+
+RPlus.Pages.Messages.patterns = [/^\/my\/messages/i];
+
+
+// WebGL3D
