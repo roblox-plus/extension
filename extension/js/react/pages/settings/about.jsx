fc41e78b83f78f5a40d115f74604241a3e44e694
WebGL3D <OgreLandBob@gmail.com>
2019-09-16 21:37:42 -0700

    Add privacy policy

--- extension/js/react/pages/settings/about.jsx
+++ extension/js/react/pages/settings/about.jsx
@@ -250,6 +250,87 @@ class About extends React.Component {
 						<p class="text-date-hint"><span class="icon-warning"></span> <span>Deprecated - These features are no longer supported. If they stop working they may not be fixed.</span></p>
 					</div>
 				</div>
+				<div class="section rplus-privacy-policy">
+					<div class="container-header">
+						<h3>Privacy Policy</h3>
+					</div>
+					<div class="section-content">
+						<span class="text-description">If you're going to use Roblox+ you should be informed of everything it does. Safety first!</span>
+						<div class="rbx-divider"></div>
+						<h4>Website Access</h4>
+						<p class="text-description">
+							<span>Any extension that has permission to roblox.com has just as much access to the website as you do! Roblox+ is no exception to that. Any extension that has access to roblox.com has access to anything you can do, including but not limited to:</span>
+							<br />
+							<ul>
+								<li>Your Robux</li>
+								<li>Your inventory</li>
+								<li>The games you play</li>
+								<li>The groups you're in</li>
+								<li>Your friends list</li>
+								<li>Roblox Authentication</li>
+								<li>Everything.</li>
+							</ul>
+							<span>Why does Roblox+ need access to all of this?</span>
+							<br />
+							<span>For the most part it doesn't. <b>None of this data is stored.</b> However - some of it used by the extension.</span>
+							<br />
+							<span>For example: The extension gets your Robux count every few seconds when you turn on the live navigation counters feature.</span>
+							<br />
+							<span>Another example: The extension loads your friends list every few seconds when you turn on the friend notifications so it knows who to notify you for.</span>
+							<br />
+							<span>Final example: Roblox authentication tokens are accessed by this extension to launch you into games when you click buttons like the follow button on notifications when your friends play a game.</span>
+							<br />
+							<span>This extension heavily utilizes your Roblox account data to improve your Roblox experience. That's the entire point of the extension. If you do not trust me as a developer to use this data securely and with good judgement to not expose you to any harm please do not use this extension.</span>
+							<br />
+							<br />
+							<span>Roblox+ needs access to the roblox.com website to be able to modify the website. Without access to the website there is no way to add these features to website.</span>
+							<br />
+							<span>Roblox+ has access to two other domains:</span>
+							<br />
+							<ul>
+								<li>rbxcdn.com</li>
+								<li><a class="text-link" href="https://roblox.plus">roblox.plus</a></li>
+							</ul>
+							<span>What are these domains?</span>
+							<br />
+							<span>rbxcdn.com is Roblox's content delivery domain. Any time the extension plays a sound (like when you get a notification) or you view asset contents on an item details page the data is loaded from an rbxcdn.com domain.</span>
+							<br />
+							<span>What about roblox.plus?</span>
+							<br />
+							<span>roblox.plus is a domain owned by me (<a class="text-link" href="/users/48103520/profile">WebGL3D</a>). This domain is used for two features of the extension:</span>
+							<br />
+							<ul>
+								<li>Roblox+ Premium</li>
+								<li>Roblox Catalog Notifier</li>
+							</ul>
+							<span>To check whether or not you are a subscriber of Roblox+ Premium a request is sent to roblox.plus which includes your user Id so the Roblox+ servers can check your Roblox+ Premium subscription status. This is only done if it cannot find a subscription by checking VIP servers for the Roblox+ Hub.</span>
+							<br />
+							<span>There is another time your user Id will be sent to the Roblox+ servers: Roblox Catalog Notifier notifications. Your user Id and cloud messaging token are sent to the Roblox+ servers to subscribe you to the cloud notification topics that are responsible for sending you notifications about Roblox catalog items.</span>
+							<br />
+							<span>Why does the Roblox Catalog Notifier need your user Id?</span>
+							<br />
+							<span>There is a process for deciding which notifications to send to you based on whether or not you're a subscriber of Roblox+ Premium. If you have Roblox+ Premium and the catalog item that gets released is limited there will be an additional purchase option directly on the popup notification. The user id is sent to the backend to know which notification topic to subscribe you to.</span>
+						</p>
+						<div class="rbx-divider"></div>
+						<h4>Data</h4>
+						<p class="text-description">
+							<span>For the most part all data for the extension remains in the extension. All personal and Roblox data is kept inside the extension and is not sent outside of Roblox.</span>
+							<br />
+							<span>There is one exception to this... your user Id. When you use the Roblox Catalog Notifier your user Id is paired with your cloud messaging token from the extension to the Roblox+ servers to send you notifications based on whether or not you have Roblox+ Premium. See above section on website access for slightly more information on this. This is the only Roblox account information that is sent to a non-Roblox server.</span>
+						</p>
+						<div class="rbx-divider"></div>
+						<h4>Web Request Interception</h4>
+						<p class="text-description">
+							<span>Some requests to roblox.com are intercepted!</span>
+							<br />
+							<span>Yup. This extension uses webRequest and webRequestBlocking permissions to intercept and modify some requests that go to Roblox. For example: To load the authentication token for game launch additional headers are needed that are not typically accessible via XMLHttpRequest. webRequest is needed for this to add the additional request parameters necessary to launch you into game as... you!</span>
+							<br />
+							<span>There are other requests being intercepted (like knowing when you visit Roblox for the first time for the extension start notification when enabled) and I could list them all out but imagine being me for a second... what if I missed one? What if I add a new feature and forget to update the privacy policy? I don't know what's legally required or not. How much do I need to specify or forget to specify before Google pulls me off the chrome web store? Instead of me trying to go into the implementation details of every feature this extension has I invite you to the extensions source.</span>
+							<br />
+							<span class="text-secondary">Roblox+ source code: <a class="text-link" href="https://git.roblox.plus/Chrome">https://git.roblox.plus</a></span>
+						</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
