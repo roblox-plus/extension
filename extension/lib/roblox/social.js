e04f1664db59a8a6e3926ce9fab2fe19e569580f
WebGL3D <OgreLandBob@gmail.com>
2020-01-18 09:53:04 -0800

    Remove unused methods from social.js

--- extension/lib/roblox/social.js
+++ extension/lib/roblox/social.js
@@ -5,38 +5,6 @@ var Roblox = Roblox || {};
 
 Roblox.social = (function () {
 	return {
-		getBlockedUsers: $.promise.cache(function (resolve, reject) {
-			$.get("https://www.roblox.com/my/settings/json").done(function (r) {
-				var users = [];
-				r.BlockedUsersModel.BlockedUsers.forEach(function (user) {
-					users.push({
-						id: user.uid,
-						username: user.Name
-					});
-				});
-				resolve(users);
-			}).fail(function () {
-				reject([{
-					code: 0,
-					message: "HTTP request failed"
-				}]);
-			});
-		}, {
-			resolveExpiry: 60 * 1000
-		}),
-		isBlocked: $.promise.cache(function (resolve, reject, userId) {
-			this.getBlockedUsers().then(function (blockedUsers) {
-				for (var n in blockedUsers) {
-					if (blockedUsers[n].id == userId) {
-						resolve(true);
-						return;
-					}
-				}
-				resolve(false);
-			}, reject);
-		}, {
-			resolveExpiry: 60 * 1000
-		}),
 		followUser: $.promise.cache(function (resolve, reject, userId) {
 			if (typeof (userId) != "number" || userId <= 0) {
 				reject([{
