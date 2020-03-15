34f2b149c5abb009c48e842e10f137f82aa1a338
WebGL3D <OgreLandBob@gmail.com>
2020-03-15 09:45:53 -0700

    no net::ERR_CONTENT_DECODING_FAILED for me

--- extension/js/background/background.js
+++ extension/js/background/background.js
@@ -90,16 +90,15 @@ Roblox.users.getAuthenticatedUser().then(function (user) {
 		}, { urls: ["*://www.roblox.com/asset/*"] }, ["blocking"]);
 
 		// Sound is dumb sometimes: net::ERR_CONTENT_DECODING_FAILED
-		chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
-			details.requestHeaders.forEach(function (header) {
-				if (header.name === "Accept-Encoding") {
-					header.value = "gzip, deflate";
-				}
-			});
-
-			return { requestHeaders: details.requestHeaders };
+		chrome.webRequest.onBeforeRequest.addListener(function (details) {
+			let redirectUrl = details.url.replace(/c(\d)\.rbxcdn\.com/i, "c$1ak.rbxcdn.com");
+			console.log("Redirecting...", details.url, redirectUrl);
+			return {
+				redirectUrl: redirectUrl
+			};
 		}, {
 			urls: [
+				"*://c0.rbxcdn.com/*",
 				"*://c1.rbxcdn.com/*",
 				"*://c2.rbxcdn.com/*",
 				"*://c3.rbxcdn.com/*",
@@ -109,9 +108,7 @@ Roblox.users.getAuthenticatedUser().then(function (user) {
 				"*://c7.rbxcdn.com/*"
 			]
 		}, [
-			"blocking",
-			"requestHeaders",
-			"extraHeaders"
+			"blocking"
 		]);
 
 		chrome.webRequest.onHeadersReceived.addListener(function (details) {
