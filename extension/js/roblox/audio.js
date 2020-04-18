b38ce8ba10210a5ac896af163d50acb8b54334cf
WebGL3D <OgreLandBob@gmail.com>
2020-04-18 16:49:05 -0700

    roblox/audi.js -> Extension.BackgroundService

--- extension/js/roblox/audio.js
+++ extension/js/roblox/audio.js
@@ -1,91 +1,100 @@
 ﻿/*
 	roblox/audio.js [03/18/2017]
 */
-(window.Roblox || (Roblox = {})).audio = (function () {
-	var getSoundUrl = $.promise.cache(function (resolve, reject, assetId) {
-		Roblox.content.getAssetContentUrl(assetId).then(resolve, reject);
-	}, {
-		resolveExpiry: 30 * 60 * 1000,
-		rejectExpiry: 2 * 60 * 1000
-	});
+var Roblox = Roblox || {};
+Roblox.Services = Roblox.Services || {};
+Roblox.Services.Audio = class extends Extension.BackgroundService {
+	constructor() {
+		super("Roblox.audio");
 
+		this.register([
+			this.getSoundUrl
+		]);
+	}
 	
-	if (ext.isContentScript) {
-		var soundMap = {};
-		$(function () {
-			var dataName = ext.id + "-robloxsound";
-			var attrName = "data-" + dataName;
-
-			$("body").on("click", ".icon-play[" + attrName + "]", function () {
-				var id = Number($(this).attr(attrName));
-				var volume = Number($(this).attr("data-volume"));
-				var player = soundMap[id];
-				var searchAttr = "[" + attrName + "='" + id + "']";
-
-				if (typeof (volume) != "number") {
-					volume = .5;
-				}
-
-				if (player) {
-					player.play(volume);
-				} else if (id <= 0) {
-					return;
-				} else {
-					$(".icon-play" + searchAttr).addClass("icon-audio").removeClass("icon-play");
-					Roblox.audio.getSoundPlayer(id).then(function (player) {
-						soundMap[id] = player;
-						player.play(function () {
-							$("span" + searchAttr).addClass("icon-pause").removeClass("icon-play icon-audio");
-						}).stop(function () {
-							$("span" + searchAttr).addClass("icon-play").removeClass("icon-pause icon-audio");
-						}).error(function () {
-							$("span" + searchAttr).addClass("icon-brokenpage").removeClass("icon-play icon-audio icon-pause");
-						});
-						player.play(volume);
-					}).catch(function(e) {
-						console.error(e);
-						$("span" + searchAttr).addClass("icon-brokenpage").removeClass("icon-play icon-audio icon-pause");
-					});
-				}
-			}).on("click", ".icon-pause[" + attrName + "]", function () {
-				var player = soundMap[$(this).attr(attrName)];
-				if (player) {
-					player.pause();
-				}
-			});
+	getSoundUrl(assetId) {
+		return CachedPromise("Roblox.audio.getSoundUrl", (resolve, reject) => {
+			Roblox.content.getAssetContentUrl(assetId).then(resolve).catch(reject);
+		}, [assetId], {
+			resolveExpiry: 30 * 60 * 1000,
+			rejectExpiry: 2 * 60 * 1000
 		});
 	}
 
-	return {
-		getSoundUrl: getSoundUrl,
+	getSoundPlayer(assetId) {
+		return new Promise((resolve, reject) => {
+			this.getSoundUrl(assetId).then((url) => {
+				resolve(soundService(url));
+			}).catch(reject);
+		});
+	}
 
-		getSoundPlayer: function (assetId) {
-			return new Promise(function(resolve, reject){
-				getSoundUrl(assetId).then(function (url) {
-					resolve(soundService(url));
-				}, reject);
-			});
-		},
+	createPlayButton(defaultVolume) {
+		if (typeof (defaultVolume) != "number") {
+			defaultVolume = .5;
+		}
 
-		createPlayButton: function (defaultVolume) {
-			if (typeof (defaultVolume) != "number") {
-				defaultVolume = .5;
+		let button = $("<span class=\"icon-play\">");
+
+		button.setAudioId = function (assetId) {
+			return button.attr("data-" + ext.id + "-robloxsound", assetId).addClass("icon-play").removeClass("icon-pause icon-audio icon-brokenpage");
+		};
+
+		button.setVolume = function (v) {
+			return button.attr("data-volume", v);
+		};
+
+		button.setVolume(defaultVolume);
+		return button;
+	}
+};
+
+Roblox.audio = new Roblox.Services.Audio();
+
+if (Extension.Singleton.executionContextType == Extension.ExecutionContextTypes.tab) {
+	$(function () {
+		let soundMap = {};
+		let dataName = ext.id + "-robloxsound";
+		let attrName = "data-" + dataName;
+
+		$("body").on("click", ".icon-play[" + attrName + "]", function () {
+			let id = Number($(this).attr(attrName));
+			let volume = Number($(this).attr("data-volume"));
+			let player = soundMap[id];
+			let searchAttr = "[" + attrName + "='" + id + "']";
+
+			if (typeof (volume) != "number") {
+				volume = .5;
 			}
 
-			var button = $("<span class=\"icon-play\">");
-			button.setAudioId = function (assetId) {
-				return button.attr("data-" + ext.id + "-robloxsound", assetId).addClass("icon-play").removeClass("icon-pause icon-audio icon-brokenpage");
-			};
-			button.setVolume = function (v) {
-				return button.attr("data-volume", v);
-			};
-
-			button.setVolume(defaultVolume);
-			return button;
-		}
-	};
-})();
-
-Roblox.audio = $.addTrigger($.promise.background("Roblox.audio", Roblox.audio));
+			if (player) {
+				player.play(volume);
+			} else if (id <= 0) {
+				return;
+			} else {
+				$(".icon-play" + searchAttr).addClass("icon-audio").removeClass("icon-play");
+				Roblox.audio.getSoundPlayer(id).then(function (player) {
+					soundMap[id] = player;
+					player.play(function () {
+						$("span" + searchAttr).addClass("icon-pause").removeClass("icon-play icon-audio");
+					}).stop(function () {
+						$("span" + searchAttr).addClass("icon-play").removeClass("icon-pause icon-audio");
+					}).error(function () {
+						$("span" + searchAttr).addClass("icon-brokenpage").removeClass("icon-play icon-audio icon-pause");
+					});
+					player.play(volume);
+				}).catch(function(e) {
+					console.error(e);
+					$("span" + searchAttr).addClass("icon-brokenpage").removeClass("icon-play icon-audio icon-pause");
+				});
+			}
+		}).on("click", ".icon-pause[" + attrName + "]", function () {
+			var player = soundMap[$(this).attr(attrName)];
+			if (player) {
+				player.pause();
+			}
+		});
+	});
+}
 
 // WebGL3D
