a60c016de183c2730ad446a24acd04bd786cbc4f
WebGL3D <OgreLandBob@gmail.com>
2020-04-19 11:34:56 -0700

    Remove soundService.robloxSound.button

--- extension/js/jquery/content.js
+++ extension/js/jquery/content.js
@@ -7,86 +7,6 @@ fixCB(({
 	"text/html": function (subdomain, upath, list) {
 		if (document.querySelector("#navigation .rplus-icon") || (["help", "corp", "developer", "wiki", "devforum", "blog", "api", "m", "bloxcon", "setup", "content", "polls"]).indexOf(subdomain = subdomain[subdomain.length - 3]) >= 0) { return; }
 		
-		function plusSlider(input) {
-			return input.each(function () {
-				var max = Number($(this).attr("max")) || 100;
-				var v = math.bound(round($(this).val(), $(this).attr("step") || 1), $(this).attr("min") || 0, max);
-				var perc = Math.floor((v / max) * 100);
-				$(this).attr("title", max > 10 ? perc + "%" : v).css("background", "-webkit-linear-gradient(left, " + plusSlider.activeColor + ", " + plusSlider.activeColor + " " + perc + "%, " + plusSlider.backgroundColor + " " + perc + "%, " + plusSlider.backgroundColor + ")");
-			});
-		}
-		plusSlider.selector = "input.rplusslider[type='range']";
-		plusSlider.activeColor = "rgb(31,115,255)";
-		plusSlider.backgroundColor = "rgb(175,175,175)";
-
-
-		soundService.robloxSound.button = function (id, tag) {
-			var v2, button, volume, state, connect, sound, stopper;
-			if (v2 = !!document.querySelector(".container-main>.content")) {
-				button = $("<" + (tag || "div") + " class=\"rplusaudio icon-audio\">");
-			} else {
-				button = $("<" + (tag || "div") + " class=\"rplusaudio MediaPlayerIcon Play\">");
-			}
-			button.append(volume = plusSlider($("<input type=\"range\" step=\"1\" min=\"0\" max=\"100\" class=\"rplusslider\">").change(function () {
-				var v = (Number($(this).val()) || 0) / 100;
-				if (sound) {
-					sound.volume(v);
-				}
-				storage.set("mediaVolume", v);
-			}))).click(function (e) {
-				e.preventDefault();
-				if (sound && e.currentTarget == e.target) {
-					if (sound.playing()) {
-						state("Play");
-					}
-					sound.volume((Number(volume.val()) || 0) / 100)[sound.playing() ? "stop" : "play"]();
-				}
-			});
-			state = function (c) { setTimeout(function () { button.attr("class", v2 ? "rplusaudio MediaPlayerIcon icon-" + (c == "Error" ? "brokenpage" : c.toLowerCase()) : "rplusaudio MediaPlayerIcon " + (c == "Audio" ? "Error" : c)); }, 10); };
-			var afterSound = function (s) {
-				sound = s;
-				state("Play");
-				s.play(function () {
-					state("Pause");
-					stopper = setInterval(function () {
-						if (!$("body").find(button).length) {
-							s.stop();
-						}
-					}, 500);
-				}).stop(function () {
-					if (stopper) {
-						clearInterval(stopper);
-					}
-					state("Play");
-				}).error(function () {
-					state("Error");
-				});
-			};
-			connect = function (i) {
-				soundService.robloxSound(i, function (s) {
-					if (s) {
-						afterSound(s);
-					} else {
-						state(Number(i) === 0 ? "Audio" : "Error");
-					}
-				});
-			};
-			if (type(id) == "string") {
-				afterSound(soundService(id, true));
-			} else {
-				connect(type(id) == "number" ? id : id.change(function () { connect(id.val()); }).val());
-			}
-			storage.get("mediaVolume", function (v) {
-				if (type(v) == "number") {
-					plusSlider(volume.val(Math.min(v * 100, 100)));
-					if (sound) {
-						sound.volume(v);
-					}
-				}
-			});
-			return button;
-		};
-
 		storage.get("twemoji", function (enabled) {
 			if (!enabled) {
 				return;
@@ -116,20 +36,7 @@ fixCB(({
 			emojiObserver.observe(document.body, { childList: true, subtree: true });
 		});
 
-		$("body").on("change", "*[storage]", function (s, v) {
-			s = $(this).attr("storage"), v = $(this).val();
-			if ($(this).attr("type") == "checkbox") {
-				storage.set(s, $(this).prop("checked"));
-			} else if ($(this).prop("tagName") == "SELECT") {
-				storage.set(s, v);
-			} else {
-				console.warn("Unhandled storage attempt", s, v);
-			}
-		}).on("input", plusSlider.selector, function () {
-			plusSlider($(this));
-		}).on("change", plusSlider.selector, function () {
-			plusSlider($(this));
-		}).on("click", ".friend-status.icon-game", function (e) {
+		$("body").on("click", ".friend-status.icon-game", function (e) {
 			e.preventDefault();
 			var id = Roblox.users.getIdFromUrl($(this).parent().parent().find(">.friend-link").attr("href"));
 			if (id) {
@@ -141,9 +48,6 @@ fixCB(({
 			RPlus.quickInfo.trigger(Roblox.users.getProfileUrl(Number($(this).attr("userid"))));
 		});
 
-		plusSlider($(plusSlider.selector));
-
-		//$(".MediaPlayerIcon[data-mediathumb-url]").each(function () { $(this).replaceWith(soundService.robloxSound.button($(this).attr("data-mediathumb-url"), $(this).prop("tagName"))); });
 		$("head").append($("<script>").attr("src", ext.getUrl("/js/jquery/overwrite.js")));
 	}
 })[document.contentType])(window.location.hostname.split("."), url.send(), []);
