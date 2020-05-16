8f7d789c9c3f239141a2c349a8030b9131c42298
WebGL3D <OgreLandBob@gmail.com>
2020-05-16 09:46:04 -0700

    Use groups api for group shout notifier

--- extension/js/background/groupShoutNotifier.js
+++ extension/js/background/groupShoutNotifier.js
@@ -1,42 +1,59 @@
 ﻿/* background/notifiers/groupShoutNotifier.js [06/04/2017] */
 RPlus.notifiers.groupShout = (function () {
+	const getGroupWhitelist = function() {
+		return new Promise(function(resolve, reject) {
+			Extension.Storage.Singleton.get("groupShoutNotifier_mode").then(mode => {
+				if (mode !== "whitelist") {
+					resolve(null);
+					return;
+				}
+
+				Extension.Storage.Singleton.get("groupShoutNotifierList").then(whitelist => {
+					resolve(whitelist || {});
+				}).catch(reject);
+			}).catch(reject);
+		});
+	};
+
 	return RPlus.notifiers.init({
 		name: "Group Shout",
 		sleep: 15 * 1000,
 		isEnabled: function (callBack) {
-			callBack(storage.get("groupShoutNotifier"));
+			Extension.Storage.Singleton.get("groupShoutNotifier").then(function(on) {
+				callBack(on);
+			}).catch(function(e) {
+				console.warn(e);
+				callBack(false);
+			});
 		},
 		requireAuthenticatedUser: true
 	}, function (user, cache, rerun) {
 		return new Promise(function (resolve, reject) {
-			$.get("https://www.roblox.com/Feeds/GetUserFeed").done(function (r) {
-				var whitelist = storage.get("groupShoutNotifierList") || {};
-				var groups = {};
-				var cacheTimestamp = cache.timestamp || 0;
-
-				$._(r).find(".feeds>.list-item").each(function () {
-					var group = $(this).find(".list-content>a[href*='/groups/']");
-					if (group.length) {
-						var shout = $(this).find(".feedtext.linkify").text();
-						shout = shout.substring(1, shout.length - 1);
-						var timestamp = new Date($(this).find(".text-date-hint").text().replace(/\s*\|\s*/g, " ")).getTime();
-						var groupId = Roblox.groups.getIdFromUrl(group.attr("href"));
-
-						if (groupId < 0 || groups[groupId] || (cache.timestamp && timestamp <= cache.timestamp)) {
+			getGroupWhitelist().then(whitelist => {
+				$.get(`https://groups.roblox.com/v1/users/${user.id}/groups/roles`).done((r) => {
+					let cacheTimestamp = cache.timestamp || 0;
+					r.data.forEach(group => {
+						let shout = group.group.shout;
+						let timestamp = shout ? new Date(shout.updated).getTime() : 0;
+						if (cache.timestamp && timestamp <= cache.timestamp) {
 							return;
 						}
 
 						cacheTimestamp = Math.max(cacheTimestamp, timestamp);
-						groups[groupId] = true;
-
-						if (!rerun
-							|| (storage.get("groupShoutNotifier_mode") === "whitelist" && !whitelist.hasOwnProperty(groupId))) {
+						if (!rerun || !shout.body) {
 							return;
 						}
 
-						var links = [];
-						var buttons = [];
-						foreach(shout.match(url.roblox.linkify) || [], function (n, o) {
+						if (whitelist && !whitelist[group.group.id]) {
+							console.log("Skipping group shout notification because the group is not whitelisted", group.group);
+							return;
+						} else {
+							console.log("RPlus.notifiers.groupShout", group.group);
+						}
+
+						let links = [];
+						let buttons = [];
+						foreach(shout.body.match(url.roblox.linkify) || [], function (n, o) {
 							if (buttons.length < 2) {
 								links.push(o);
 								buttons.push("Visit link " + links.length);
@@ -45,34 +62,39 @@ RPlus.notifiers.groupShout = (function () {
 							}
 						});
 						
-						$.notification({
-							tag: "groupshout" + groupId,
-							title: group.text(),
-							message: string.clean(shout.replace(/https?:\/\/w?w?w?\./gi, "")),
-							icon: $(this).find(".header-thumb").attr("src"),
-							buttons: buttons,
-							clickable: true,
-							metadata: {
-								robloxSound: Number((storage.get("notifierSounds") || {}).groupShout) || 0,
-								url: group.attr("href"),
-								speak: "Group shout from " + group.text()
-							}
-						}).click(function () {
-							this.close();
-						}).buttonClick(function (index) {
-							window.open(links[index]);
+						Extension.Storage.Singleton.get("notifierSounds").then(notifierSounds => {
+							Roblox.thumbnails.getGroupIconUrl(group.group.id, 420, 420).then(groupIconUrl => {
+								$.notification({
+									tag: "groupshout" + group.group.id,
+									title: group.group.name,
+									message: string.clean(shout.body.replace(/https?:\/\/w?w?w?\./gi, "")),
+									icon: groupIconUrl,
+									buttons: buttons,
+									clickable: true,
+									metadata: {
+										robloxSound: Number((notifierSounds || {}).groupShout) || 0,
+										url: Roblox.groups.getGroupUrl(group.group.id, group.group.name),
+										speak: "Group shout from " + group.group.name
+									}
+								}).click(function () {
+									this.close();
+								}).buttonClick(function (index) {
+									window.open(links[index]);
+								});
+							}).catch(e => {
+								console.warn("failed to load group icon for notification", e, group);
+							});
+						}).catch(e => {
+							console.warn(e);
 						});
-					}
-				});
+					});
 
-				cache.timestamp = cacheTimestamp;
-				resolve([]);
-			}).fail(function () {
-				reject([{
-					code: 0,
-					message: "HTTP request failed"
-				}]);
-			});
+					cache.timestamp = cacheTimestamp;
+					resolve([]);
+				}).fail((jxhr, errors) => {
+					reject(errors);
+				});
+			}).catch(reject);
 		});
 	});
 })();
