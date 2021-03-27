cbae6ead56679acb90bd3fb636d328f88fca8ec9
WebGL3D <OgreLandBob@gmail.com>
2021-03-27 14:33:24 -0700

    Remove duplicate group role display

--- extension/js/jquery/GroupV2.js
+++ extension/js/jquery/GroupV2.js
@@ -10,26 +10,6 @@ RPlus.Pages.Group = function (pathMatch) {
 
 	console.log("Group:", id);
 
-	Extension.Storage.Singleton.get("groupRoleDisplay").then(function(groupRoleDisplayEnabled) {
-		if (!groupRoleDisplayEnabled) {
-			return;
-		}
-
-		setInterval(function () {
-			$(".group-comments .list-body .text-date-hint:not([rplus])").attr("rplus", "").each(function () {
-				var el = $(this);
-				var posterId = Roblox.users.getIdFromUrl(el.closest(".comment.list-item").find("a[href*='/users/']").attr("href"));
-				if (posterId) {
-					Roblox.groups.getUserRole(id, posterId).then(function (role) {
-						el.prepend($("<span>").text(role.name), " | ");
-					}).catch(function (e) {
-						console.error(e);
-					});
-				}
-			});
-		}, 500);
-	}).catch(console.warn);
-
 	if (id == 650266) {
 		setInterval(function () {
 			$(".group-comments .group-menu .dropdown-menu:not([rplus])").attr("rplus", "").each(function () {
