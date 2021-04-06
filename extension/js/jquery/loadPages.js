59202caf7a9d3038ba03da42df81c77f10c16f74
WebGL3D <OgreLandBob@gmail.com>
2021-04-06 13:31:03 -0700

    Attempt to fix React bug

--- extension/js/jquery/loadPages.js
+++ extension/js/jquery/loadPages.js
@@ -1,13 +1,11 @@
 ﻿var RPlus = RPlus || {};
 RPlus.Pages = RPlus.Pages || {};
 
-(function () {
-	// TODO: Remove chat-data-model as source of truth, it appears this has been removed.
-	Roblox.users.authenticatedUserId = $("meta[name='user-data']").data("userid")
-		|| $("#chat-data-model").data("userid")
-		|| $("#push-notification-registrar-settings").data("userid")
-		|| Number((($(".TradeWindowContainer>script").html() || "").match(/Offer\.initialize\((\d+),/) || [0, 0])[1])
-		|| 0;
+RPlus.Pages.init = function() {
+	if (!window.React) {
+		setTimeout(RPlus.Pages.init, 10);
+		return;
+	}
 
 	for (var pageName in RPlus.Pages) {
 		if (RPlus.Pages.hasOwnProperty(pageName)) {
@@ -34,6 +32,17 @@ RPlus.Pages = RPlus.Pages || {};
 	}
 	
 	RPlus.style.syncPremiumClass();
+};
+
+(function () {
+	// TODO: Remove chat-data-model as source of truth, it appears this has been removed.
+	Roblox.users.authenticatedUserId = $("meta[name='user-data']").data("userid")
+		|| $("#chat-data-model").data("userid")
+		|| $("#push-notification-registrar-settings").data("userid")
+		|| Number((($(".TradeWindowContainer>script").html() || "").match(/Offer\.initialize\((\d+),/) || [0, 0])[1])
+		|| 0;
+
+	RPlus.Pages.init();
 })();
 
 
