551a00c26d6270eb84371e64e096ace902c31fbd
WebGL3D <WebGL3D@users.noreply.github.com>
2022-12-25 13:29:12 -0800

    Add usersService.getUserByName

--- src/js/services/usersService.ts
+++ src/js/services/usersService.ts
@@ -28,40 +28,86 @@ const _getAuthenticatedUser = BatchedPromise<User | null>(
 // Using BatchedPromise as a means to cache resolves/rejections, even though there is no actual input.
 const getAuthenticatedUser = () => _getAuthenticatedUser(0);
 
-const getUserById = BatchedPromise<User | null>({}, async (userIds) => {
-  const response = await fetch('https://users.roblox.com/v1/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
+const getUserById = BatchedPromise<User | null>(
+  {
+    cacheDuration: 60 * 1000,
+  },
+  async (userIds) => {
+    const response = await fetch('https://users.roblox.com/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userIds,
+        excludeBannedUsers: false,
+      }),
+    });
+
+    const result = await response.json();
+
+    return translateOutput(
       userIds,
-      excludeBannedUsers: false,
-    }),
-  });
+      result.data,
+      (user: User) => user.id,
+      (user: User) =>
+        user
+          ? {
+              id: user.id,
+              name: user.name,
+              displayName: user.displayName,
+            }
+          : null
+    );
+  }
+);
 
-  const result = await response.json();
+const _getUserByName = BatchedPromise<User | null>(
+  {
+    cacheDuration: 60 * 1000,
+  },
+  async (usernames) => {
+    const response = await fetch(
+      'https://users.roblox.com/v1/usernames/users',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          usernames,
+          excludeBannedUsers: false,
+        }),
+      }
+    );
 
-  return translateOutput(
-    userIds,
-    result.data,
-    (user: User) => user.id,
-    (user: User) =>
-      user
-        ? {
-            id: user.id,
-            name: user.name,
-            displayName: user.displayName,
-          }
-        : null
-  );
-});
+    const result = await response.json();
+
+    return translateOutput(
+      usernames,
+      result.data,
+      (user: any) => user.requestedUsername,
+      (user: User) =>
+        user
+          ? {
+              id: user.id,
+              name: user.name,
+              displayName: user.displayName,
+            }
+          : null
+    );
+  }
+);
+
+// Translate it so that the
+const getUserByName = (username: string) =>
+  _getUserByName(username.toLowerCase());
 
 // Export + attach to global
 declare global {
   var usersService: any;
 }
 
-globalThis.usersService = { getAuthenticatedUser, getUserById };
+globalThis.usersService = { getAuthenticatedUser, getUserById, getUserByName };
 
-export { getAuthenticatedUser, getUserById };
+export { getAuthenticatedUser, getUserById, getUserByName };
