6b34e00dee0b3282d918387f93b2540352e26405
WebGL3D <WebGL3D@users.noreply.github.com>
2022-12-25 15:53:10 -0800

    Read authenticated user directly from loaded content
    May reverse this decision later.

--- src/js/services/usersService.ts
+++ src/js/services/usersService.ts
@@ -6,6 +6,22 @@ const _getAuthenticatedUser = BatchedPromise<User | null>(
     cacheDuration: 15 * 1000,
   },
   async (_) => {
+    if (document) {
+      // TODO: Think about this logic.. on one hand it saves us requests, because the data is in the page load.
+      // On the other hand, the user could log out in one tab, this tab may not reload, and then log in as someone else,
+      // and then suddenly this returns the wrong data.
+      const userData = document.querySelector(`meta[name='user-data']`);
+      if (userData) {
+        return [
+          {
+            id: Number(userData.getAttribute('data-userid')),
+            name: userData.getAttribute('data-name'),
+            displayName: userData.getAttribute('data-displayname'),
+          },
+        ];
+      }
+    }
+
     const response = await fetch(
       'https://users.roblox.com/v1/users/authenticated'
     );
