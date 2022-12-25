2fbcdef8ba4234a822e79cfd684a5a0482c1516f
WebGL3D <WebGL3D@users.noreply.github.com>
2022-12-25 12:09:49 -0800

    Scrub everything
    Prepare for a clean slate..

--- extension/js/jquery/loadStylesheets.js
+++
@@ -1,8 +0,0 @@
-﻿(function () {
-	RPlus.style.init();
-	RPlus.style.loadStylesheet(chrome.extension.getURL("/css/output/themes/dark-theme.css"));
-	RPlus.style.loadStylesheet(chrome.extension.getURL("/css/rplus/images.css"));
-	RPlus.style.loadStylesheet(chrome.extension.getURL("/css/customIcon.css"));
-})();
-
-// WebGL3D
