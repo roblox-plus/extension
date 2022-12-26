bf60f17ac933c5bb227063c0bd3c8b54d45e23ca
WebGL3D <WebGL3D@users.noreply.github.com>
2022-12-25 19:26:51 -0800

    Add back game vote percentage feature
    This has probably been broken for a long time.
    
    Should this be behind a setting?

--- src/js/pages/game-details/index.ts
+++ src/js/pages/game-details/index.ts
@@ -49,22 +49,53 @@ const badgesCheck = async (authenticatedUser: User) => {
     });
 };
 
-const pageLoaded = (authenticatedUser: User) => {
+const updateVoteTitle = (
+  upvoteSpan: Element | null,
+  downvoteSpan: Element | null
+) => {
+  const upvoteCount = Number(upvoteSpan?.getAttribute('title'));
+  const downvoteCount = Number(downvoteSpan?.getAttribute('title'));
+  if (isNaN(upvoteCount) || isNaN(downvoteCount)) {
+    return;
+  }
+
+  upvoteSpan?.setAttribute('title', upvoteCount.toLocaleString());
+  downvoteSpan?.setAttribute('title', downvoteCount.toLocaleString());
+
+  const percentage =
+    downvoteCount < 1
+      ? 100
+      : (100 / (upvoteCount + downvoteCount)) * upvoteCount;
+
+  if (upvoteSpan?.parentElement?.parentElement) {
+    const percentageSpan = document.createElement('span');
+    percentageSpan.classList.add('count-middle');
+    percentageSpan.setAttribute(
+      'title',
+      `${
+        percentage === 100 ? 100 : percentage.toFixed(3)
+      }% of players recommend this game`
+    );
+    percentageSpan.innerText = `${Math.floor(percentage)}%`;
+
+    upvoteSpan.parentElement.after(percentageSpan);
+  }
+};
+
+const pageLoaded = (authenticatedUser: User | null) => {
   setInterval(async () => {
-    try {
-      await badgesCheck(authenticatedUser);
-    } catch (e) {
-      console.error(e);
+    updateVoteTitle(
+      document.querySelector('#vote-up-text'),
+      document.querySelector('#vote-down-text')
+    );
+    if (authenticatedUser) {
+      try {
+        await badgesCheck(authenticatedUser);
+      } catch (e) {
+        console.error(e);
+      }
     }
   }, 1000);
 };
 
-getAuthenticatedUser()
-  .then((authenticatedUser) => {
-    if (!authenticatedUser) {
-      return;
-    }
-
-    pageLoaded(authenticatedUser);
-  })
-  .catch(console.error);
+getAuthenticatedUser().then(pageLoaded).catch(console.error);
