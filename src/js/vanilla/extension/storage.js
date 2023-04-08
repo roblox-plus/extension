2b6ac1419e2f7bfd3ea5c240fd132c519c1336be
WebGL3D <WebGL3D@users.noreply.github.com>
2023-04-08 12:28:46 -0700

    Format storage.js

--- src/js/vanilla/extension/storage.js
+++ src/js/vanilla/extension/storage.js
@@ -1,79 +1,85 @@
 Extension.Storage = class extends Extension.BackgroundService {
-	constructor() {
-		super("Extension.Storage");
+  constructor() {
+    super('Extension.Storage');
 
-		this.register([
-			this.get,
-			this.set,
-			this.remove
-		]);
-	}
+    this.register([this.get, this.set, this.remove]);
+  }
 
-	get(key) {
-		return new Promise((resolve, reject) => {
-			let value = null;
-			try {
-				const proposedValue = this.getSync(key);
-				if (proposedValue !== undefined) {
-					value = proposedValue;
-				}
-			} catch (e) {
-				reject(e);
-				return;
-			}
-			
-			resolve(value);
-		});
-	}
+  get(key) {
+    return new Promise((resolve, reject) => {
+      let value = null;
+      try {
+        const proposedValue = this.getSync(key);
+        if (proposedValue !== undefined) {
+          value = proposedValue;
+        }
+      } catch (e) {
+        reject(e);
+        return;
+      }
 
-	getSync(key) {
-		if (Extension.Singleton.executionContextType !== Extension.ExecutionContextTypes.background) {
-			throw new Error("Extension.Storage.getSync only available in the background page");
-		}
+      resolve(value);
+    });
+  }
 
-		let rawValue = localStorage.getItem(key);
-		if (typeof(rawValue) === "string") {
-			let valueArray = JSON.parse(rawValue);
-			if (Array.isArray(valueArray) && valueArray.length > 0) {
-				return valueArray[0];
-			}
-		}
-	}
+  getSync(key) {
+    if (
+      Extension.Singleton.executionContextType !==
+      Extension.ExecutionContextTypes.background
+    ) {
+      throw new Error(
+        'Extension.Storage.getSync only available in the background page'
+      );
+    }
 
-	set(key, value) {
-		return new Promise((resolve, reject) => {
-			try {
-				let serializedValue = JSON.stringify([value]);
-				localStorage.setItem(key, serializedValue);
-			} catch (e) {
-				reject(e);
-				return;
-			}
-			
-			resolve({});
-		});
-	}
+    let rawValue = localStorage.getItem(key);
+    if (typeof rawValue === 'string') {
+      let valueArray = JSON.parse(rawValue);
+      if (Array.isArray(valueArray) && valueArray.length > 0) {
+        return valueArray[0];
+      }
+    }
+  }
 
-	remove(key) {
-		return new Promise((resolve, reject) => {
-			try {
-				localStorage.removeItem(key);
-			} catch (e) {
-				reject(e);
-				return;
-			}
-			
-			resolve({});
-		});
-	}
+  set(key, value) {
+    return new Promise((resolve, reject) => {
+      try {
+        let serializedValue = JSON.stringify([value]);
+        localStorage.setItem(key, serializedValue);
+      } catch (e) {
+        reject(e);
+        return;
+      }
 
-	blindSet(key, value) {
-		this.set(key, value).then(() => {
-			// set successful yay
-		}).catch(err => {
-			console.warn(`Failed to set value (Extension.Storage) for key (${key}):`, err);
-		});
-	}
+      resolve({});
+    });
+  }
+
+  remove(key) {
+    return new Promise((resolve, reject) => {
+      try {
+        localStorage.removeItem(key);
+      } catch (e) {
+        reject(e);
+        return;
+      }
+
+      resolve({});
+    });
+  }
+
+  blindSet(key, value) {
+    this.set(key, value)
+      .then(() => {
+        // set successful yay
+      })
+      .catch((err) => {
+        console.warn(
+          `Failed to set value (Extension.Storage) for key (${key}):`,
+          err
+        );
+      });
+  }
 };
 
 Extension.Storage.Singleton = new Extension.Storage();
