70a34055e1679be4ffec017380e21926a4439460
WebGL3D <WebGL3D@users.noreply.github.com>
2023-04-15 08:59:58 -0700

    Remove loadStylesheet
    no longer in use

--- src/js/jquery/style.js
+++ src/js/jquery/style.js
@@ -3,28 +3,6 @@ RPlus.style = RPlus.style || (function () {
 	var premiumSubscriberClass = "rplus-premium-subscriber";
 	var darkThemeClass = "dark-theme";
 
-	function loadStylesheet(stylesheetLocation) {
-		fetch(stylesheetLocation).then(function (response) {
-			response.text().then(function (originalCss) {
-				var css = originalCss.replace(/url\(([^)]+)\)/gi, function (u) {
-					var url = u.substring(4, u.length - 1);
-					if (url.match(/^\w+:/)) {
-						return u;
-					} else {
-						return "url(" + chrome.extension.getURL(url) + ")";
-					}
-				});
-				var styleTag = document.createElement("style");
-				styleTag.innerHTML = css;
-				document.head.appendChild(styleTag);
-			}).catch(function (e) {
-				console.warn("Failed to read stylesheet\n\t", stylesheetLocation, "\n\t", e);
-			});
-		}).catch(function (e) {
-			console.warn("Failed to load stylesheet\n\t", stylesheetLocation, "\n\t", e);
-		});
-	}
-
 	function syncNavigationTheme() {
 		if (window.$) {
 			if (!document.documentElement.classList.contains(premiumSubscriberClass)) {
@@ -66,7 +44,6 @@ RPlus.style = RPlus.style || (function () {
 	}
 
 	return {
-		loadStylesheet: loadStylesheet,
 		init: init,
 		togglePremiumClass: togglePremiumClass,
 		syncPremiumClass: syncPremiumClass
