9e09837eb19de800ed7d0c0ba8f04fbdbb475545
WebGL3D <WebGL3D@users.noreply.github.com>
2023-04-29 21:42:43 -0700

    Introduce services/friends

---
+++ src/js/services/friends/getUserFriends.ts
@@ -0,0 +1,49 @@
+import User from '../../types/user';
+import ExpirableDictionary from '../../utils/expireableDictionary';
+import { addListener, sendMessage } from '../message';
+
+const messageDestination = 'friendsService.getUserFriends';
+const cache = new ExpirableDictionary<User[]>(messageDestination, 60 * 1000);
+
+// The type for the message being passed to the background.
+type BackgroundMessage = {
+  userId: number;
+};
+
+// Fetches the list of friends for the user.
+const getUserFriends = (userId: number): Promise<User[]> => {
+  return sendMessage(messageDestination, {
+    userId,
+  } as BackgroundMessage);
+};
+
+// Loads the actual friend list for the user.
+const loadUserFriends = async (userId: number): Promise<User[]> => {
+  const response = await fetch(
+    `https://friends.roblox.com/v1/users/${userId}/friends`
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed to load friends for user (${userId})`);
+  }
+
+  const result = await response.json();
+  return result.data.map((r: any) => {
+    return {
+      id: r.id,
+      name: r.name,
+      displayName: r.displayName,
+    };
+  });
+};
+
+// Listen for messages of things trying to fetch presence.
+addListener(messageDestination, (message: BackgroundMessage) => {
+  // Check the cache
+  return cache.getOrAdd(`${message.userId}`, () =>
+    // Queue up the fetch request, when not in the cache
+    loadUserFriends(message.userId)
+  );
+});
+
+export default getUserFriends;
