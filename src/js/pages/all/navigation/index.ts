67559867a050fdb873129b2827d53d17d9c66adb
WebGL3D <WebGL3D@users.noreply.github.com>
2023-04-30 11:42:38 -0700

    Finish porting the navigation bar -> TypeScript

--- src/js/pages/all/navigation/index.ts
+++ src/js/pages/all/navigation/index.ts
@@ -1,9 +1,11 @@
+import { manifest } from '../../../constants';
 import { getFriendRequestCount } from '../../../services/friends';
 import { getUnreadMessageCount } from '../../../services/private-messages';
 import { getToggleSettingValue } from '../../../services/settings';
 import { getTradeCount } from '../../../services/trades';
 import { parseAuthenticatedUser } from '../../../utils/authenticatedUser';
 import { getBubbleValue, setBubbleValue } from './bubble';
+import { getLinkOverrides, updateNavigationLink } from './links';
 import { getRobux, setRobux } from './robux';
 
 // Check if we should be refreshing the counter values.
@@ -74,6 +76,65 @@ setInterval(async () => {
   // Update the trade count.
   const trades = await getTradeBubbleCount(shouldRefresh);
   setBubbleValue('nav-trade', trades);
+
+  // Update navigation links.
+  const links = await getLinkOverrides();
+  if (links.length === 2) {
+    // First element in the array is the third link in the navigation bar.
+    // Which is also the link that is second to last.
+    if (links[0].override) {
+      updateNavigationLink(-2, links[0].text, links[0].href);
+    }
+
+    // Second element in the array is the fourth link in the navigation bar.
+    // Which is also the link that is also the last link in the navigation bar.
+    if (links[1].override) {
+      updateNavigationLink(-1, links[1].text, links[1].href);
+    }
+  }
+
+  // Control panel link.
+  let controlPanelLink = document.querySelector(
+    'a#nav-rplus'
+  ) as HTMLAnchorElement;
+  if (!controlPanelLink && manifest.homepage_url) {
+    const upgradeButton = document.querySelector('li.rbx-upgrade-now');
+    if (!upgradeButton) {
+      // Couldn't find the element we use to prepend before... :coffin:
+      return;
+    }
+
+    const container = document.createElement('li');
+
+    // The link itself
+    controlPanelLink = document.createElement('a');
+    controlPanelLink.setAttribute('id', 'nav-rplus');
+    controlPanelLink.setAttribute('href', manifest.homepage_url);
+    controlPanelLink.setAttribute(
+      'class',
+      'dynamic-overflow-container text-nav'
+    );
+    container.appendChild(controlPanelLink);
+
+    // The icon
+    const controlPanelIcon = document.createElement('span');
+    controlPanelIcon.setAttribute('class', 'rplus-icon');
+    const controlPanelIconContainer = document.createElement('div');
+    controlPanelIconContainer.appendChild(controlPanelIcon);
+    controlPanelLink.appendChild(controlPanelIconContainer);
+
+    // The text
+    const controlPanelText = document.createElement('span');
+    controlPanelText.setAttribute(
+      'class',
+      'font-header-2 dynamic-ellipsis-item'
+    );
+    controlPanelText.innerText = 'Control Panel';
+    controlPanelLink.appendChild(controlPanelText);
+
+    // The finale
+    upgradeButton.before(container);
+  }
 }, 500);
 
 // Export to window, for debugging purposes.
@@ -86,4 +147,5 @@ window.navigationBar = {
   setRobux,
   getBubbleValue,
   setBubbleValue,
+  updateNavigationLink,
 };
