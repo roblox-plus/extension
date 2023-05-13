88d5b94789e9febadfb70bb742b3d17bc84a63aa
WebGL3D <WebGL3D@users.noreply.github.com>
2023-05-13 09:47:59 -0700

    Remove game server pager
    This was intended to have been removed with #46.

--- src/js/jquery/Game.js
+++ src/js/jquery/Game.js
@@ -15,62 +15,7 @@ RPlus.Pages.Game = function () {
 	}
 	console.log("Game creator id", gameCreatorId);
 
-	var servers;
-	servers = {
-		anchor: $("<a>"),
-		page: function () { return Number(servers.curPage.text()) || 1; },
-		maxPage: $("<span>1</span>"),
-		curPage: $("<span>1</span>"),
-		getMaxPage: function() {
-			return Number(servers.maxPage.text()) || 1;
-		},
-		firstPage: $("<li class=\"first disabled\">").append("<a><span class=\"icon-first-page\"></span></a>").click(function () {
-			if (!$(this).hasClass("disabled")) {
-				servers.fetch(1);
-			}
-		}),
-		lastPage: $("<li class=\"last disabled\">").append("<a><span class=\"icon-last-page\"></span></a>").click(function () {
-			if (!$(this).hasClass("disabled")) {
-				servers.fetch(servers.getMaxPage());
-			}
-		}),
-		prevPage: $("<li class=\"pager-prev disabled\">").append("<a><span class=\"icon-left\"></span></a>").click(function () {
-			if (!$(this).hasClass("disabled")) {
-				var newPage = Math.min(servers.getMaxPage(), Math.max(1, servers.page() - 1));
-				servers.fetch(newPage);
-			}
-		}),
-		nextPage: $("<li class=\"pager-next disabled\">").append("<a><span class=\"icon-right\"></span></a>").click(function () {
-			if (!$(this).hasClass("disabled")) {
-				servers.fetch(Math.min(Number(servers.maxPage.text()) || 1, servers.page() + 1));
-			}
-		}),
-		fetch: function (p) {
-			if (type(p) == "number") {
-				servers.curPage.text(p.toString());
-			}
-
-			servers.firstPage.toggleClass("disabled", servers.page() <= 1);
-			servers.prevPage.toggleClass("disabled", servers.page() <= 1);
-			servers.anchor.attr("href", "javascript:Roblox.RunningGameInstances.fetchServers(" + placeId + "," + ((servers.page() - 1) * 10) + ");")[0].click();
-		}
-	};
-
-	$(".rbx-running-games-footer").html("").append($("<ul class=\"pager\">")
-		.append(servers.firstPage)
-		.append(servers.prevPage)
-		.append($("<li class=\"pager-cur\"></li>").append(servers.curPage))
-		.append($("<li class=\"pager-total\"><span class=\"fixed-spacing\">of</span></li>").append(servers.maxPage))
-		.append(servers.nextPage)
-		.append(servers.lastPage));
-
 	$(".rbx-gear-item-delete>.icon-delete").addClass("icon-alert");
-	
-	$(".rbx-running-games-refresh").click(function () {
-		servers.curPage.text("1");
-		servers.firstPage.addClass("disabled");
-		servers.prevPage.addClass("disabled");
-	});
 	
 	if (placeId === 258257446) {
 		$(".create-server-banner-text").text("Purchasing a VIP server in this place will activate Roblox+ Premium!\nRoblox+ currently only expands the Roblox dark theme onto pages unsupported by Roblox.\n\tThere are plans to expand Roblox+ Premium in the future, but not in the short term.");
