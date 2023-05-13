4f37c5c139267be7f03972652fbefcbb9ac7a380
WebGL3D <WebGL3D@users.noreply.github.com>
2023-05-13 14:58:34 -0700

    sales counter is premium only
    maintenance is time, and time is valuable

--- src/js/react/pages/settings/other-settings.jsx
+++ src/js/react/pages/settings/other-settings.jsx
@@ -39,9 +39,12 @@ class OtherSettings extends SettingsTab {
 						<PillToggle getValue={this.getPillValue.bind(this, "remainingCounter")}
 							onToggle={this.setPillValue.bind(this, "remainingCounter")} />
 						<div class="rbx-divider"></div>
-						<span class="text-lead">Sales counter on created items.</span>
+						<span class="text-lead">Created Items Sales Counter</span>
 						<PillToggle getValue={this.getPillValue.bind(this, "itemSalesCounter")}
-							onToggle={this.setPillValue.bind(this, "itemSalesCounter")} />
+							onToggle={this.setPillValue.bind(this, "itemSalesCounter")}
+							disabled={!this.state.isPremium} />
+						<div class="rbx-divider"></div>
+						<span class="text-description">Adds a sales count label on item details pages for items that you created. This feature is for Roblox+ Premium users only.</span>
 					</div>
 				</div>
 				<div class="section">
