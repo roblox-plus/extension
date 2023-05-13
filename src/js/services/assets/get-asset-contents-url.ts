35075dabc4cd84d1f4e17b5c4e76bc8604f123d1
WebGL3D <WebGL3D@users.noreply.github.com>
2023-05-13 13:06:45 -0700

    Create assetsService

---
+++ src/js/services/assets/get-asset-contents-url.ts
@@ -0,0 +1,92 @@
+import { Batch, BatchItem } from '@tix-factory/batch';
+import ExpirableDictionary from '../../utils/expireableDictionary';
+import xsrfFetch from '../../utils/xsrfFetch';
+import { manifest } from '../../constants';
+import { addListener, sendMessage } from '../message';
+
+const messageDestination = 'assetsService.getAssetContentsUrl';
+
+// The type for the message being passed to the background.
+type BackgroundMessage = {
+  assetId: number;
+};
+
+class AssetContentsBatchProcessor extends Batch<number, string> {
+  constructor() {
+    super({
+      levelOfParallelism: 1,
+      maxSize: 100,
+      minimumDelay: 1000,
+      enqueueDeferDelay: 10,
+    });
+  }
+
+  async process(items: BatchItem<number, string>[]) {
+    const requestHeaders = new Headers();
+    requestHeaders.append('Roblox-Place-Id', '258257446');
+    requestHeaders.append('Roblox-Browser-Asset-Request', manifest.name);
+
+    const response = await xsrfFetch(
+      new URL(`https://assetdelivery.roblox.com/v2/assets/batch`),
+      {
+        method: 'POST',
+        headers: requestHeaders,
+        body: JSON.stringify(
+          items.map((batchItem) => {
+            return {
+              assetId: batchItem.value,
+              requestId: batchItem.key,
+            };
+          })
+        ),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to load asset contents URL');
+    }
+
+    const result = await response.json();
+    items.forEach((item) => {
+      const asset = result.data.find((a: any) => a.RequestId === item.key);
+
+      const location = asset?.Locations[0];
+      if (location?.location) {
+        item.resolve(location.location);
+      } else {
+        item.resolve('');
+      }
+    });
+  }
+
+  getKey(item: number): string {
+    return item.toString();
+  }
+}
+
+const assetContentsProcessor = new AssetContentsBatchProcessor();
+const assetContentsCache = new ExpirableDictionary<string>(
+  messageDestination,
+  10 * 60 * 1000
+);
+
+// Fetches the date when a badge was awarded to the specified user.
+const getAssetContentsUrl = (assetId: number): Promise<string> => {
+  return sendMessage(messageDestination, {
+    assetId,
+  } as BackgroundMessage);
+};
+
+// Listen for messages sent to the service worker.
+addListener(messageDestination, (message: BackgroundMessage) => {
+  // Check the cache
+  return assetContentsCache.getOrAdd(
+    assetContentsProcessor.getKey(message.assetId),
+    () => {
+      // Queue up the fetch request, when not in the cache
+      return assetContentsProcessor.enqueue(message.assetId);
+    }
+  );
+});
+
+export default getAssetContentsUrl;
---
+++ src/js/services/assets/index.ts
@@ -0,0 +1,4 @@
+import getAssetContentsUrl from './get-asset-contents-url';
+
+// To ensure the webpack service loader can discover the methods: import it, then export it again.
+export { getAssetContentsUrl };
