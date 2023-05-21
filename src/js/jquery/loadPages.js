4dc8399b3555e939e901133691b271a640a9631a
Bob Rose <OgreLandBob@gmail.com>
2023-05-21 11:31:15 -0700

    Remove Item.js
    Won't be relevant by the end of this PR.

--- src/js/jquery/loadPages.js
+++ src/js/jquery/loadPages.js
@@ -7,34 +7,6 @@ RPlus.Pages.init = function() {
 		return;
 	}
 
-	for (let pageName in RPlus.Pages) {
-		if (RPlus.Pages.hasOwnProperty(pageName)) {
-			if (pageName === "init") {
-				continue;
-			}
-
-			if (typeof (RPlus.Pages[pageName]) != "function") {
-				console.warn("Miss-indexed page value:", pageName);
-			}
-
-			if(!RPlus.Pages[pageName].hasOwnProperty("patterns") || !Array.isArray(RPlus.Pages[pageName].patterns)){
-				console.warn("Missing pattern:", pageName);
-				continue;
-			}
-
-			var patterns = RPlus.Pages[pageName].patterns;
-			for (var i = 0; i < patterns.length; i++) {
-				if (patterns[i].test(location.pathname)) {
-					console.log("Page:", pageName);
-					var patternMatch = location.pathname.match(patterns[i]);
-					RPlus.Pages[pageName] = RPlus.Pages[pageName](patternMatch);
-					RPlus.Pages[pageName].patterns = patterns;
-					break;
-				}
-			}
-		}
-	}
-	
 	RPlus.style.syncPremiumClass();
 };
 
