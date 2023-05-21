b4432191c8f5afe9795fe37c5448bb155651df49
Bob Rose <WebGL3D@users.noreply.github.com>
2023-05-21 09:33:51 -0700

    Remove notification stream (#86)

--- src/js/service-worker/index.ts
+++ src/js/service-worker/index.ts
@@ -1,1 +1,13 @@
+import { manifest } from '../constants';
 export * from './notifiers';
+
+chrome.browserAction.setTitle({
+  title: `${manifest.name} ${manifest.version}`,
+});
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.tabs.create({
+    url: manifest.homepage_url,
+    active: true,
+  });
+});
