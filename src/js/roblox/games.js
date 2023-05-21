b2cb1803903d8b2fd750ca4d5148d0854721f9ce
Bob Rose <OgreLandBob@gmail.com>
2023-05-21 14:09:41 -0700

    Remove unused methods

--- src/js/roblox/games.js
+++ src/js/roblox/games.js
@@ -8,8 +8,6 @@ Roblox.Services.Games = class extends Extension.BackgroundService {
     super('Roblox.games');
 
     this.register([
-      this.getVipServerById,
-      this.getVipServers,
       this.getGroupGames,
     ]);
   }
@@ -34,93 +32,6 @@ Roblox.Services.Games = class extends Extension.BackgroundService {
     }
 
     return `https://www.roblox.com/games/${placeId}/${placeName}`;
-  }
-
-  getVipServerById(vipServerId) {
-    return CachedPromise(
-      `${this.serviceId}.getVipServers`,
-      (resolve, reject) => {
-        $.get(`https://games.roblox.com/v1/vip-servers/${vipServerId}`)
-          .done((r) => {
-            const expirationDate = new Date(r.subscription.expirationDate);
-            resolve({
-              id: r.id,
-              name: r.name,
-              expirationDate: expirationDate.getTime(),
-            });
-          })
-          .fail(Roblox.api.$reject(reject));
-      },
-      [vipServerId],
-      {
-        queued: true,
-        resolveExpiry: 15 * 1000,
-        rejectExpiry: 10 * 1000,
-      }
-    );
-  }
-
-  getVipServers(placeId, cursor) {
-    return CachedPromise(
-      `${this.serviceId}.getVipServers`,
-      (resolve, reject) => {
-        Roblox.users
-          .getAuthenticatedUser()
-          .then((authenticatedUser) => {
-            $.get(
-              `https://games.roblox.com/v1/games/${placeId}/private-servers`,
-              {
-                limit: 100,
-                cursor: cursor || '',
-              }
-            )
-              .done((r) => {
-                const vipServers = [];
-                const vipServerPromises = [];
-
-                r.data.forEach((server) => {
-                  if (server.owner.id === authenticatedUser.id) {
-                    vipServerPromises.push(
-                      this.getVipServerById(server.vipServerId).then(
-                        (vipServer) => {
-                          vipServer.owner = server.owner;
-                          vipServers.push(vipServer);
-                        }
-                      )
-                    );
-                  } else {
-                    vipServers.push({
-                      id: server.vipServerId,
-                      name: server.name,
-                      owner: server.owner,
-                    });
-                  }
-                });
-
-                Promise.all(vipServerPromises)
-                  .then(() => {
-                    if (r.nextPageCursor) {
-                      this.getVipServers(placeId, r.nextPageCursor)
-                        .then((moreVipServers) => {
-                          resolve(vipServers.concat(moreVipServers));
-                        })
-                        .catch(reject);
-                    } else {
-                      resolve(vipServers);
-                    }
-                  })
-                  .catch(reject);
-              })
-              .fail(Roblox.api.$reject(reject));
-          })
-          .catch(reject);
-      },
-      [placeId, cursor],
-      {
-        resolveExpiry: 15 * 1000,
-        rejectExpiry: 10 * 1000,
-      }
-    );
   }
 
   getGroupGames(groupId) {
