bf95987101d7d48a5a38c1e5bba68fbba0b4988d
WebGL3D <WebGL3D@users.noreply.github.com>
2023-05-27 11:09:13 -0700

    Prevent duplicate authentication requests

--- services/web-app/src/js/services/authentication/index.ts
+++ services/web-app/src/js/services/authentication/index.ts
@@ -1,50 +1,71 @@
 import { apiBaseUrl } from '../../constants';
 import User from '../../types/user';
 
-let authenticatedUser: User | null | undefined;
+let authenticatedUserPromise: Promise<User | null>;
+const loginsByCode: { [code: string]: Promise<User> } = {};
 
 // Gets the currently authenticated user.
-const getAuthenticatedUser = async (): Promise<User | null> => {
-  if (authenticatedUser !== undefined) {
-    return authenticatedUser;
-  }
+const getAuthenticatedUser = (): Promise<User | null> => {
+  return (
+    authenticatedUserPromise ||
+    (authenticatedUserPromise = new Promise(async (resolve, reject) => {
+      try {
+        const response = await fetch(
+          `${apiBaseUrl.href}v1/users/authenticated`,
+          {
+            credentials: 'include',
+          }
+        );
 
-  const response = await fetch(`${apiBaseUrl.href}v1/users/authenticated`, {
-    credentials: 'include',
-  });
-
-  switch (response.status) {
-    case 200:
-      const payload = await response.json();
-      return (authenticatedUser = payload.data);
-    case 401:
-      return (authenticatedUser = null);
-    default:
-      throw new Error(
-        'Failed to determined authenticated user, please refresh the page to try again.'
-      );
-  }
+        switch (response.status) {
+          case 200:
+            resolve(await response.json());
+            return;
+          case 401:
+            resolve(null);
+            return;
+          default:
+            throw new Error(
+              'Failed to determined authenticated user, please refresh the page to try again.'
+            );
+        }
+      } catch (e) {
+        reject(e);
+      }
+    }))
+  );
 };
 
 // Logs in the user with their authorization code.
-const login = async (code: string): Promise<User> => {
-  const response = await fetch(`${apiBaseUrl.href}v1/login`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      data: code,
-    }),
-  });
-
-  if (response.status === 200) {
-    const result = await response.json();
-    return result.data;
+const login = (code: string): Promise<User> => {
+  const loginPromise = loginsByCode[code];
+  if (loginPromise) {
+    return loginPromise;
   }
 
-  throw new Error('Login failed, please try again.');
+  return (loginsByCode[code] = new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(`${apiBaseUrl.href}v1/login`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data: code,
+        }),
+      });
+
+      if (response.status === 200) {
+        resolve(await response.json());
+        return;
+      }
+
+      throw new Error('Login failed, please try again.');
+    } catch (e) {
+      reject(e);
+    }
+  }));
 };
 
 // Logs the user out.
