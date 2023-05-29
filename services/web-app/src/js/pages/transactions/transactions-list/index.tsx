099611fd4d863c261d24cec114e4931415d51241
WebGL3D <WebGL3D@users.noreply.github.com>
2023-05-29 07:25:54 -0700

    Add header to transaction items

--- services/web-app/src/js/pages/transactions/transactions-list/index.tsx
+++ services/web-app/src/js/pages/transactions/transactions-list/index.tsx
@@ -10,7 +10,7 @@ import {
   Typography,
 } from '@mui/material';
 import useSelectedCreator from '../hooks/useSelectedCreator';
-import { useEffect, useMemo, useState } from 'react';
+import { Fragment, useEffect, useMemo, useState } from 'react';
 import { getTransactionsByOwner } from '../../../services/transactions';
 import LoadingState from '../../../enums/loadingState';
 import Transaction from '../../../types/transaction';
@@ -154,66 +154,81 @@ export default function TransactionsList({
   }, [creator, startDate, endDate]);
 
   return (
-    <Box className="transactions-panel" sx={{ flexGrow: 1, flexWrap: 'wrap' }}>
-      {loadingState === LoadingState.Loading ? <CircularProgress /> : null}
-      {loadingState === LoadingState.Error ? (
-        <Alert severity="error">
-          Failed to load items owned by the selected creator. Please refresh the
-          page to try again.
-        </Alert>
-      ) : null}
-      {items.map((item) => {
-        return (
-          <TransactionCardContainer
-            link={item.link}
-            key={`${item.type}:${item.id}`}
-          >
-            <CardMedia
-              className="rplus-item-card-media"
-              data-item-type={item.type}
-              data-item-id={item.id}
-              sx={{
-                height: '128px',
-                width: '128px',
-                p: 1,
-              }}
-            />
-            <CardContent
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                flexGrow: 1,
-                overflow: 'hidden',
-              }}
+    <Fragment>
+      <Typography variant="h4" sx={{ width: '100%', float: 'left', pl: 1 }}>
+        Transaction Items
+      </Typography>
+      <Typography
+        variant="subtitle2"
+        sx={{ width: '100%', float: 'left', pl: 1 }}
+      >
+        These are all the items sold in the selected time window, sorted by the
+        top selling.
+      </Typography>
+      <Box
+        className="transactions-panel"
+        sx={{ flexGrow: 1, flexWrap: 'wrap' }}
+      >
+        {loadingState === LoadingState.Loading ? <CircularProgress /> : null}
+        {loadingState === LoadingState.Error ? (
+          <Alert severity="error">
+            Failed to load items owned by the selected creator. Please refresh
+            the page to try again.
+          </Alert>
+        ) : null}
+        {items.map((item) => {
+          return (
+            <TransactionCardContainer
+              link={item.link}
+              key={`${item.type}:${item.id}`}
             >
-              <Typography
-                variant="h6"
+              <CardMedia
+                className="rplus-item-card-media"
+                data-item-type={item.type}
+                data-item-id={item.id}
                 sx={{
-                  textOverflow: 'ellipsis',
-                  whiteSpace: 'nowrap',
+                  height: '128px',
+                  width: '128px',
+                  p: 1,
+                }}
+              />
+              <CardContent
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  flexGrow: 1,
                   overflow: 'hidden',
                 }}
-                title={item.name}
               >
-                {item.name}
-              </Typography>
-              <Typography variant="subtitle2">{item.type}</Typography>
-              <List>
-                <ListItem sx={{ p: 0 }}>
-                  <ListItemText>
-                    Sales: {item.sales.toLocaleString()}
-                  </ListItemText>
-                </ListItem>
-                <ListItem sx={{ p: 0 }}>
-                  <ListItemText>
-                    Revenue: {item.revenue.toLocaleString()}
-                  </ListItemText>
-                </ListItem>
-              </List>
-            </CardContent>
-          </TransactionCardContainer>
-        );
-      })}
-    </Box>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                  }}
+                  title={item.name}
+                >
+                  {item.name}
+                </Typography>
+                <Typography variant="subtitle2">{item.type}</Typography>
+                <List>
+                  <ListItem sx={{ p: 0 }}>
+                    <ListItemText>
+                      Sales: {item.sales.toLocaleString()}
+                    </ListItemText>
+                  </ListItem>
+                  <ListItem sx={{ p: 0 }}>
+                    <ListItemText>
+                      Revenue: {item.revenue.toLocaleString()}
+                    </ListItemText>
+                  </ListItem>
+                </List>
+              </CardContent>
+            </TransactionCardContainer>
+          );
+        })}
+      </Box>
+    </Fragment>
   );
 }
