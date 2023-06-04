621642a6fd2b122e9a2e43193e90be40ae23df63
WebGL3D <WebGL3D@users.noreply.github.com>
2023-06-03 17:28:45 -0700

    Add terms of service, and privacy policy

---
+++ services/web-app/src/js/pages/privacy-policy/index.tsx
@@ -0,0 +1,7 @@
+import { Navigate } from 'react-router-dom';
+
+export default function PrivacyPolicy() {
+  return (
+    <Navigate to="https://github.com/roblox-plus/extension/blob/master/PrivacyPolicy.md" />
+  );
+}
