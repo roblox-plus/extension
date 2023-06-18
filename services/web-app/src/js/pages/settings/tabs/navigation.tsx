9fed2c9567d7bd065722d35bf3fb69240a42265b
WebGL3D <WebGL3D@users.noreply.github.com>
2023-06-18 12:32:32 -0700

    Add setting for rounded navigation counters

--- services/web-app/src/js/pages/settings/tabs/navigation.tsx
+++ services/web-app/src/js/pages/settings/tabs/navigation.tsx
@@ -2,6 +2,7 @@ import { Typography } from '@mui/material';
 import { Fragment } from 'react';
 import Emoji from '../../../components/emoji';
 import ToggleCard from '../components/toggle-card';
+import NavigationCounterRoundingSetting from './navigation-counter-rounding';
 
 export default function NavigationSettings() {
   return (
@@ -19,6 +20,7 @@ export default function NavigationSettings() {
         description="Your Robux, private message, trade, and friend request counts will be refreshed periodically, without refreshing the page."
         settingName="navcounter"
       />
+      <NavigationCounterRoundingSetting />
     </Fragment>
   );
 }
