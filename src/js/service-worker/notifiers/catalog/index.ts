94656b7f5d2ac3bfb7a05ae9885f7bd2001f14f0
WebGL3D <WebGL3D@users.noreply.github.com>
2023-06-25 15:49:28 -0700

    Port catalog notifier to TypeScript

--- src/js/service-worker/notifiers/catalog/index.ts
+++ src/js/service-worker/notifiers/catalog/index.ts
@@ -1,7 +1,10 @@
+import { isAuthenticatedUserFollowing } from '../../../services/followings';
 import { getToggleSettingValue } from '../../../services/settings';
-import { getAuthenticatedUser } from '../../../services/users';
+import { getAuthenticatedUser, getUserByName } from '../../../services/users';
+import fetchDataUri from '../../../utils/fetchDataUri';
 
 const tokenRefreshInterval = 30 * 60 * 1000;
+const notificationIdPrefix = 'catalog_notifier:';
 
 const isEnabled = (): Promise<boolean> => {
   return getToggleSettingValue('itemNotifier');
@@ -48,6 +51,88 @@ const updateToken = (): Promise<void> => {
   });
 };
 
+const shouldShowNotification = async (
+  creatorName?: string
+): Promise<boolean> => {
+  // This logic is no longer valid, but still in use. It doesn't support group creators, it assumes all creators are users that can be followed.
+  // As a result: No notifications for group-created items will be shown.
+  if (!creatorName) {
+    // If there's no creator on the notification, it is assumed to be created by the Roblox account.
+    // And of course everyone wants these notifications.. right?
+    return true;
+  }
+
+  const authenticatedUser = await getAuthenticatedUser();
+  if (!authenticatedUser) {
+    // Not logged in, no notification.
+    return false;
+  }
+
+  if (authenticatedUser.name === creatorName) {
+    // Of course you always want to see your own notifications.
+    return true;
+  }
+
+  const creator = await getUserByName(creatorName);
+  if (!creator) {
+    // Couldn't determine who the user is, so no notification will be visible. Cool.
+    return false;
+  }
+
+  // And the final kicker... you can only see notifications if you follow the creator.
+  const isFollowing = await isAuthenticatedUserFollowing(creator.id);
+  return isFollowing;
+};
+
+const processNotification = async (notification: any): Promise<any> => {
+  const showNotification = await shouldShowNotification(
+    notification.items?.Creator
+  );
+  if (!showNotification) {
+    console.log(
+      'Skipping notification, likely because the authenticated user does not follow the creator',
+      notification
+    );
+    return;
+  }
+
+  const requireProperties = ['icon', 'url', 'title', 'message'];
+  for (let i = 0; i < requireProperties.length; i++) {
+    if (!notification[requireProperties[i]]) {
+      console.warn(
+        `Skipping notification because there is no ${requireProperties[i]}`,
+        notification
+      );
+      return;
+    }
+  }
+
+  const iconUrl = await fetchDataUri(notification.icon);
+  const notificationOptions: chrome.notifications.NotificationOptions<true> = {
+    type: 'basic',
+    iconUrl,
+    title: notification.title,
+    message: notification.message,
+  };
+
+  if (notification.items && Object.keys(notification.items).length > 0) {
+    notificationOptions.type = 'list';
+    notificationOptions.items = [];
+    for (let title in notification.items) {
+      notificationOptions.items.push({
+        title,
+        message: notification.items[title],
+      });
+    }
+  }
+
+  chrome.notifications.create(
+    `${notificationIdPrefix}${notification.url}`,
+    notificationOptions,
+    () => {}
+  );
+};
+
 const processMessage = async (message: any): Promise<void> => {
   try {
     const enabled = await isEnabled();
@@ -56,6 +141,22 @@ const processMessage = async (message: any): Promise<void> => {
     }
 
     console.log('Processing gcm message', message);
+
+    switch (message.from) {
+      case '/topics/catalog-notifier':
+      case '/topics/catalog-notifier-premium':
+        if (!message.data?.notification) {
+          console.warn('Failed to parse gcm message notification', message);
+          return;
+        }
+
+        await processNotification(JSON.parse(message.data.notification));
+
+        return;
+      default:
+        console.warn('Unknown gcm message sender', message);
+        return;
+    }
   } catch (err) {
     console.error('Failed to process gcm message', err, message);
   }
@@ -64,6 +165,26 @@ const processMessage = async (message: any): Promise<void> => {
 // @ts-ignore:next-line: https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/65809
 chrome.instanceID.onTokenRefresh.addListener(updateToken);
 chrome.gcm.onMessage.addListener(processMessage);
+
+chrome.notifications.onClicked.addListener((notificationId) => {
+  if (!notificationId.startsWith(notificationIdPrefix)) {
+    return;
+  }
+
+  const url = notificationId.substring(notificationIdPrefix.length);
+  if (!url.startsWith('https://www.roblox.com/')) {
+    console.warn(
+      'Skipped opening URL for notification because it was not for roblox.com',
+      notificationId
+    );
+    return;
+  }
+
+  chrome.tabs.create({
+    url,
+    active: true,
+  });
+});
 
 export default async function (
   nextTokenUpdate: number | null
