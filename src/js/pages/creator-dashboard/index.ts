b5890d197d7ce5bfcb8daa52d8820e8677592422
Bob Rose <WebGL3D@users.noreply.github.com>
2024-06-22 10:09:28 -0700

    Remove creator dashboard features

--- src/js/pages/creator-dashboard/index.ts
+++
@@ -1,3 +0,0 @@
-// Because create.roblox.com doesn't actually load new pages when links are clicked, we have to load
-// all extension features at once, and periodically check if features need to be loaded in.
-export * from './developer-stats';
--- src/js/services/premium-payouts/index.ts
+++
@@ -1,4 +0,0 @@
-import { default as getPremiumPayoutsSummary } from './getPremiumPayoutsSummary';
-
-// To ensure the webpack service loader can discover the methods: import it, then export it again.
-export { getPremiumPayoutsSummary };
